370d0e7e499da6e3cc340cb7fcb9f999d2ac5125
Nick Taylor <nick@fractaldimensions.io>
2021-11-01 13:38:42 +0000

    remove vendor

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ module.exports = function(eleventyConfig) {
   // Watch our compiled assets for changes
   eleventyConfig.addWatchTarget('./src/compiled-assets/main.css');
   eleventyConfig.addWatchTarget('./src/compiled-assets/main.js');
-  eleventyConfig.addWatchTarget('./src/compiled-assets/vendor.js');
 
   // Copy src/compiled-assets to /assets
   eleventyConfig.addPassthroughCopy({ 'src/compiled-assets': 'assets' });
